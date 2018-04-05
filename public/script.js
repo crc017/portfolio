18c95b3f39a0471fae2d6d55de6ed2160bb5f977
Charles Cox <charlescox@Charless-MacBook-Pro.local>
2018-04-05 01:17:10 -0400

    upgrade

---
+++ public/script.js
@@ -0,0 +1,29 @@
+$("#main").mouseenter(function() {
+    if($("#profileImage").hasClass("mouseEntered")==false){
+        console.log("mouseEntered class has been added");
+        $("#profileImage").addClass(
+            "mouseEntered"
+        );
+        // console.log("hover works!");
+        // $("#profileImage").fadeOut(1000, function() {
+        //     $("#profileImage").css({
+        //         filter: 'blur(0px)'
+        //     });
+        // });
+        
+        // $("#profileImage").fadeIn(2000);
+        
+        $("#profileImage").fadeOut(2000, function() {
+            $("#profileImage").attr('src', './images/johnDoe.jpg');
+            $("#profileImage").css({
+                opacity: '0.9',
+                filter: 'brightness(100%) grayscale(60%)'
+            });
+        });
+
+        $("#profileImage").fadeIn(2000);
+
+    }else{
+        console.log("mouseEntered class already exists")
+    }
+});
